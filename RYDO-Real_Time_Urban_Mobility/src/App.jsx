053d974565b2ed4224bd5a91483d7f2d13857e66
Damchey Lhendup <lhendupdamchey@gmail.com>
2026-04-27 21:03:02 +0530

    My changes in App.js

--- RYDO-Real_Time_Urban_Mobility/src/App.jsx
+++ RYDO-Real_Time_Urban_Mobility/src/App.jsx
@@ -1,6 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './Pages/LandingPage/LandingPage.jsx';
-import AuthPage from './Pages/AuthPage/AuthPage.jsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./Pages/LandingPage/LandingPage.jsx";
+import AuthPage from "./Pages/AuthPage/AuthPage.jsx";
+import SignupPage from "./Pages/SignupPage/SignupPage.jsx";
 
 function App() {
   return (
@@ -8,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
+          <Route path="/" element={<SignupPage />} />
       </Routes>
     </Router>
   );
