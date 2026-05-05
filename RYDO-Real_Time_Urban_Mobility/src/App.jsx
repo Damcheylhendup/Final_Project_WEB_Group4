3b9ca0169cb7e110dbf5ec078894ee23b1872652
NgawangNorbu123 <ngawangznorbu@gmail.com>
2026-05-05 15:46:18 +0600

    05/05/26

--- RYDO-Real_Time_Urban_Mobility/src/App.jsx
+++ RYDO-Real_Time_Urban_Mobility/src/App.jsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-
-import LandingPage from "./Pages/LandingPage/LandingPage.jsx";
-import LoginPage from "./Pages/LoginPage/LoginPage.jsx";
-import AuthPage from "./Pages/AuthPage/AuthPage.jsx";
-import DashboardPage from "./Pages/DashboardPage/DashboardPage.jsx";
-import BookingPage from "./Pages/BookingPage/BookingPage.jsx";
-import TripsPage from "./Pages/TripsPage/TripsPage.jsx";
-import PaymentsPage from "./Pages/PaymentsPage/PaymentsPage.jsx";
-import MapPage from "./Pages/MapPage/MapPage.jsx";
-import AccountSettingsPage from "./Pages/AccountSettingsPage/AccountSettingsPage.jsx";
-
-function AppWrapper() {
-=======
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage/LandingPage.jsx';
@@ -35,32 +19,24 @@ function App() {
     document.documentElement.classList.toggle('dark-mode', savedDarkMode);
   }, []);
 
->>>>>>> a4bdaca6d37f9d5974a29abebf0bdc6951cc8ccf
   return (
     <Router>
-      <App />
+      <Routes>
+  <Route path="/" element={<LandingPage />} />
+  <Route path="/landing" element={<LandingPage />} />
+  <Route path="/login" element={<LoginPage />} />
+  <Route path="/auth" element={<AuthPage />} />
+  <Route path="/create-account" element={<CreateAccountPage />} />
+  <Route path="/dashboard" element={<ProtectedRoute allowedRole="rider"><DashboardPage /></ProtectedRoute>} />
+  <Route path="/booking" element={<ProtectedRoute allowedRole="rider"><BookingPage /></ProtectedRoute>} />
+  <Route path="/trips" element={<ProtectedRoute allowedRole="rider"><TripsPage /></ProtectedRoute>} />
+  <Route path="/payments" element={<ProtectedRoute allowedRole="rider"><PaymentsPage /></ProtectedRoute>} />
+  <Route path="/map" element={<ProtectedRoute allowedRole="rider"><MapPage /></ProtectedRoute>} />
+  <Route path="/driver-dashboard" element={<ProtectedRoute allowedRole="driver"><DriverDashboardPage /></ProtectedRoute>} />
+  <Route path="/account" element={<ProtectedRoute><AccountSettingsPage /></ProtectedRoute>} />
+</Routes>
     </Router>
   );
 }
 
-  return (
-    <>
-      {loading && <Loading />}
-
-      <Routes>
-        <Route path="/landing" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/create-account" element={<CreateAccountPage />} />
-        <Route path="/dashboard" element={<ProtectedRoute allowedRole="rider"><DashboardPage /></ProtectedRoute>} />
-        <Route path="/booking" element={<ProtectedRoute allowedRole="rider"><BookingPage /></ProtectedRoute>} />
-        <Route path="/trips" element={<ProtectedRoute allowedRole="rider"><TripsPage /></ProtectedRoute>} />
-        <Route path="/payments" element={<ProtectedRoute allowedRole="rider"><PaymentsPage /></ProtectedRoute>} />
-        <Route path="/map" element={<ProtectedRoute allowedRole="rider"><MapPage /></ProtectedRoute>} />
-        <Route path="/driver-dashboard" element={<ProtectedRoute allowedRole="driver"><DriverDashboardPage /></ProtectedRoute>} />
-        <Route path="/account" element={<ProtectedRoute><AccountSettingsPage /></ProtectedRoute>} />
-      </Routes>
-    </>
-  );
-
-export default AppWrapper;
+export default App;
