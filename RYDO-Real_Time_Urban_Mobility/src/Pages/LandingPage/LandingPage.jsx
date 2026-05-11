497e861bf4a9e5255b793fb70e71864dd2a4975e
Damchey Lhendup <lhendupdamchey@gmail.com>
2026-05-11 23:14:36 +0530

    Updated landing page responsiveness

--- RYDO-Real_Time_Urban_Mobility/src/Pages/LandingPage/LandingPage.jsx
+++ RYDO-Real_Time_Urban_Mobility/src/Pages/LandingPage/LandingPage.jsx
@@ -1,24 +1,23 @@
-import { useNavigate } from 'react-router-dom';
-import './LandingPage.css';
+import "./LandingPage.css";
 
 function LandingPage() {
-  const navigate = useNavigate();
-
   return (
-    <div className="splash-screen">
-      <div className="content">
-        <h1 className="logo box">
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1 className="logo">
           <span className="yellow">RY</span>
-          <span className="red">DO</span>
+          <span className="orange">DO</span>
         </h1>
 
-        <p className="tagline">Real-Time Urban Mobility</p>
+        <h2 className="subtitle">Real-Time Urban Mobility</h2>
 
-        <button className="start-btn" onClick={() => navigate('/login')}>
-          Get Started <span className="arrow">→</span>
-        </button>
+        <div className="bottom-section">
+          <button className="start-btn">
+            Get Started <span>→</span>
+          </button>
 
-        <p className="subtag">"Get in, go out — འགྱོ་བ vibes."</p>
+          <p className="tagline">"Get in, go out — སྐྱིད vibes."</p>
+        </div>
       </div>
     </div>
   );
