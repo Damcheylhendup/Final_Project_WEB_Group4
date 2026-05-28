8d81deddad4dbe310658088b3176e8f4621ba96a
lindel19 <sonamlindel@agmail.com>
2026-05-28 22:22:42 +0600

    f

--- frontend/src/websocket/userRideSocket.js
+++ frontend/src/websocket/userRideSocket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://rydo-backend.onrender.com", {
+const socket = io("http://localhost:4000", {
   autoConnect: true,
   transports: ["polling", "websocket"],
   withCredentials: true,
